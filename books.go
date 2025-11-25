4dd46ec519b5340746a77f8dd49ba4d2d4c491d3
SirawitDev <sirawit.krsri@gmail.com>
2025-11-25 16:02:12 +0700

    update:CRUDFiber

---
+++ books.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+
+func getBooks(c *fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+func getBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	// Error handling
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	// Find book by ID
+	for _, book := range books {
+		if book.ID == bookId {
+			return c.JSON(book)
+		}
+	}
+
+	// If book not found
+	//return  c.SendStatus(fiber.StatusNotFound)
+	return  c.Status(fiber.StatusNotFound).SendString("Not found book")
+}
+
+func createBook(c *fiber.Ctx) error {
+	book := new(Book)
+
+	if err := c.BodyParser(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	books = append(books, *book)
+
+	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return  c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+
+	// Parse the request body into bookUpdate
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	// Find and update the book
+	for i, book := range books {
+		if book.ID == bookId {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			return c.JSON(books[i])
+		}
+	}
+	return  c.SendStatus(fiber.StatusNotFound)
+}
+
+func deleteBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == bookId {
+			// [1,2,3,4,5]
+			// [1,2] + [4,5] = [1,2,4,5]
+			books = append(books[:i], books[i+1:]...)
+			return c.SendStatus(fiber.StatusNoContent)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
+}
--- main.go
+++ main.go
@@ -4,12 +4,55 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Book struct to hold book data
+type Book struct {
+	ID	int `json:"id"`
+	Title string `json:"title"`
+	Author	string 	`json:"author"`
+}
+
+var books []Book // Slice to store books []Slice [1]Array
+
 func main() {
 	app := fiber.New() // app ตัวแทน Fiber application || app = express() ใน Node.js
 
-	app.Get("/hello" , func(c * fiber.Ctx) error {
-		return c.SendString("Hello Golang")
-	})
+	//initialize in-memory data
+	books = append(books, Book{ ID:1, Title:"1984", Author: "George Orwell"})
+	books = append(books, Book{ ID:2, Title:"The Great Gatsby", Author:"F. Scott Fitzgerald"})
+
+	// Apply CORS middleware
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "*", // Adjust this to be more restrictive if needed
+	// 	AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+	// 	AllowHeaders: "Origin, Content-Type, Accept",
+	// })
+
+	app.Get("/books" , getBooks)
+	app.Get("/books/:id" , getBook)
+	app.Post("/books" , createBook)
+	app.Put("/books/:id" , updateBook)
+	app.Delete("/books/:id" , deleteBook)
+
+	app.Post("/upload" , uploadFile)
 
 	app.Listen(":8080")
 }
+
+func uploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	err = c.SaveFile(file, "./uploads/" + file.Filename)
+
+	if err != nil {
+		return  c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.SendString("File uploaded successfully: " + file.Filename)
+}
+
+
+//nodemon --watch . --ext go --exec go run . --signal SIGTERM
